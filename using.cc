c057749ef274bb099fbefa2d40549ce77b378cb1
endrollex <seebook@126.com>
2013-07-20 10:34:31 +0800

    w

--- using.cc
+++ using.cc
@@ -1,35 +1,3 @@
-/*#include <iostream>
-#include <string>
-#include <iomanip>
-#include <ctime>
-#include "intbigf.h"
-using namespace std;
-int main()
-{
-	clock_t t = clock();
-	
-	intbigdata n1, n2, nres;
-	vector<char> res;
-	
-	n1.assign("12345678123456781234567812345678");
-	n2.assign("987654321987654321987654321987654321");
-	
-	//res = intbigd_fu::karatsuba(n1.bigint, n2.bigint);
-	
-	nres.assign(res);
-	
-	cout << nres << endl;
-	cout << n1*n2 << endl;
-	cout << n1.size() << endl;
-	cout << n2.size() << endl;
-	
-	t = clock() - t;
-	cout << "It took me " << ((float)t)/CLOCKS_PER_SEC << " seconds." << endl;	
-	
-	return 0;
-}
-*/
-
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -38,14 +6,6 @@ int main()
 #include <sstream>
 #include "intbigf.h"
 using namespace std;
-void frac_test(const intbigf &a)
-{
-	for (int x = 1; x != (int)a+1; ++x) {
-		intbigf y = a/(intbigf)x;
-		cout << "a = " << a << ", "<< y << "^" << x << ": " << endl;
-		cout << ((y).pow_int(x)).size() << " " << ((y).pow_int(x)) << endl;
-	}
-}
 int main()
 {
 	intbigd_fu::precision_affect_div_inf();
@@ -57,17 +17,9 @@ int main()
 	
 	clock_t t = clock();
 	
-	//frac_test(100);
+	intbigf n3 = 3000;
 	
-	
-	intbigf n1("14422");
-	intbigf n2("59720728576");
-	
-	intbigf n3 = 3;
-	
-	cout << n3 << endl;
-	cout << n3.root_int(66) << endl;
-	//cout << cbrt(3.0) << endl;
+	cout << n3.root_int(3) << endl;
 	cout << "1.4422495703074083823216383107801" << endl;
 	
 
